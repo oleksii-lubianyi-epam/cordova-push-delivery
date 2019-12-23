6576488356b080449b7b0bfedbf8f297c1d98e5d
Rostyslav Antonov <rostyslav_antonov@epam.com>
2019-12-23 11:40:34 +0200

    Fixing typo

--- androidReplaceService.js
+++ androidReplaceService.js
@@ -30,7 +30,7 @@ module.exports = function(ctx) {
 
 	//reading parameters from fetch.json
 	contents = fs.readFileSync(path.join(context.opts.projectRoot, 'plugins/fetch.json'));
-	var pluginVars = JSON.parse(contents).["com-epam-dhl-cordova-push-delivery"];
+	var pluginVars = JSON.parse(contents)["com-epam-dhl-cordova-push-delivery"];
 	console.log(pluginVars);
 
     return deferral.promise;
