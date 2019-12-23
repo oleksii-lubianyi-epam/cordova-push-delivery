df081cda46fcd452671fb2c41ed3a6eb1597d4af
Rostyslav Antonov <rostyslav_antonov@epam.com>
2019-12-23 10:07:54 +0200

    Small cleaning and experiment with parameters

--- androidReplaceService.js
+++ androidReplaceService.js
@@ -1,19 +1,19 @@
 var fs = require('fs');
 var path = require('path');
+var Q = require('q');
+var elementTree = require('elementtree');
 
 module.exports = function(ctx) {
     // need to check that Android platform added 
     if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
     }
-    var fs = ctx.requireCordovaModule('fs'),
-        path = ctx.requireCordovaModule('path'),
-        deferral = ctx.requireCordovaModule('q').defer();
+    var deferral = new Q.defer();
 
 	console.log(' Patching AndroidManifest.xml ');
     var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
     var manifest = path.join(platformRoot, 'app/src/main/AndroidManifest.xml');
-	
+
 	var contents = fs.readFileSync(manifest,
 		'utf-8'
 	);
@@ -25,7 +25,23 @@ module.exports = function(ctx) {
 	}
 	console.log(' Writing changes ');
 
-	fs.writeFileSync(manifest, "*****" + contents);
+	fs.writeFileSync(manifest, contents);
+	
+	console.log(' ************* ');
+	console.log(context.opts.plugin);
+	console.log(' ************* ');
+
+	//reading parameters from config.xml
+//	contents = fs.readFileSync(
+//		path.join(context.opts.projectRoot, 'config.xml'),
+//		'utf-8'
+//	);
+
+//	if (contents) {
+//		contents = contents.substring(contents.indexOf('<'));
+//	}
+//	var etree = elementTree.parse(contents);
+//	etree.findall('*/category/[@term="monitoring.entity.update"]'
 
     return deferral.promise;
 };
